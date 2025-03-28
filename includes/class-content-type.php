99537cbde883d230aee2310c0af59659ec52559b
prcdevgitbot <webdev@pewresearch.org>
2025-03-28 16:17:35 +0000

    See https://github.com/pewresearch/prc-platform/commit/f328c9def2a740cebe37db54f8ab2193c29c6c42 from refs/tags/v1.1.13

--- includes/class-content-type.php
+++ includes/class-content-type.php
@@ -2,62 +2,84 @@
 namespace PRC\Platform\Chart_Builder;
 
 class Content_Type {
+	/**
+	 * The post type slug.
+	 *
+	 * @var string
+	 */
 	public static $post_type = 'chart';
+	/**
+	 * The menu icon.
+	 *
+	 * @var string
+	 */
 	public static $menu_icon = 'dashicons-chart-area';
 
-	public function __construct($loader) {
-		$loader->add_action('init', $this, 'register_types');
-		$loader->add_filter('oembed_response_data', $this, 'modify_oembed_response', 10, 4);
-		$loader->add_filter('prc_load_gutenberg', $this, 'enable_gutenberg_ramp');
+	/**
+	 * The constructor.
+	 *
+	 * @param mixed $loader
+	 */
+	public function __construct( $loader ) {
+		$loader->add_action( 'init', $this, 'register_types' );
+		$loader->add_filter( 'oembed_response_data', $this, 'modify_oembed_response', 10, 4 );
 	}
 
+	/**
+	 * Get the labels for the post type.
+	 *
+	 * @return array
+	 */
 	public static function get_labels() {
-		return [
-			'name'                  => _x( 'Charts', 'Post Type General Name', PRC_CHART_BUILDER_NAMESPACE ),
-			'singular_name'         => _x( 'Chart', 'Post Type Singular Name', PRC_CHART_BUILDER_NAMESPACE ),
-			'menu_name'             => __( 'Charts', PRC_CHART_BUILDER_NAMESPACE ),
-			'name_admin_bar'        => __( 'Chart', PRC_CHART_BUILDER_NAMESPACE ),
-			'archives'              => __( 'Chart Archives', PRC_CHART_BUILDER_NAMESPACE ),
-			'parent_item_colon'     => __( 'Parent Chart:', PRC_CHART_BUILDER_NAMESPACE ),
-			'all_items'             => __( 'All Charts', PRC_CHART_BUILDER_NAMESPACE ),
-			'add_new_item'          => __( 'Add New Chart', PRC_CHART_BUILDER_NAMESPACE ),
-			'add_new'               => __( 'Add New', PRC_CHART_BUILDER_NAMESPACE ),
-			'new_item'              => __( 'New Chart', PRC_CHART_BUILDER_NAMESPACE ),
-			'edit_item'             => __( 'Edit Chart', PRC_CHART_BUILDER_NAMESPACE ),
-			'update_item'           => __( 'Update Chart', PRC_CHART_BUILDER_NAMESPACE ),
-			'view_item'             => __( 'View Chart', PRC_CHART_BUILDER_NAMESPACE ),
-			'search_items'          => __( 'Search Charts', PRC_CHART_BUILDER_NAMESPACE ),
-			'not_found'             => __( 'Not found', PRC_CHART_BUILDER_NAMESPACE ),
-			'not_found_in_trash'    => __( 'Not found in Trash', PRC_CHART_BUILDER_NAMESPACE ),
-			'featured_image'        => __( 'Chart Thumbnail', PRC_CHART_BUILDER_NAMESPACE ),
-			'set_featured_image'    => __( 'Set chart thumbnail', PRC_CHART_BUILDER_NAMESPACE ),
-			'remove_featured_image' => __( 'Remove chart thumbnail', PRC_CHART_BUILDER_NAMESPACE ),
-			'use_featured_image'    => __( 'Use as chart thumbnail', PRC_CHART_BUILDER_NAMESPACE ),
-			'insert_into_item'      => __( 'Insert into Chart', PRC_CHART_BUILDER_NAMESPACE ),
-			'uploaded_to_this_item' => __( 'Uploaded to this Chart', PRC_CHART_BUILDER_NAMESPACE ),
-			'items_list'            => __( 'Charts list', PRC_CHART_BUILDER_NAMESPACE ),
-			'items_list_navigation' => __( 'Charts list navigation', PRC_CHART_BUILDER_NAMESPACE ),
-			'filter_items_list'     => __( 'Filter Chart list', PRC_CHART_BUILDER_NAMESPACE ),
-		];
+		return array(
+			'name'                  => _x( 'Charts', 'Post Type General Name', 'prc-chart-builder' ),
+			'singular_name'         => _x( 'Chart', 'Post Type Singular Name', 'prc-chart-builder' ),
+			'menu_name'             => __( 'Charts', 'prc-chart-builder' ),
+			'name_admin_bar'        => __( 'Chart', 'prc-chart-builder' ),
+			'archives'              => __( 'Chart Archives', 'prc-chart-builder' ),
+			'parent_item_colon'     => __( 'Parent Chart:', 'prc-chart-builder' ),
+			'all_items'             => __( 'All Charts', 'prc-chart-builder' ),
+			'add_new_item'          => __( 'Add New Chart', 'prc-chart-builder' ),
+			'add_new'               => __( 'Add New', 'prc-chart-builder' ),
+			'new_item'              => __( 'New Chart', 'prc-chart-builder' ),
+			'edit_item'             => __( 'Edit Chart', 'prc-chart-builder' ),
+			'update_item'           => __( 'Update Chart', 'prc-chart-builder' ),
+			'view_item'             => __( 'View Chart', 'prc-chart-builder' ),
+			'search_items'          => __( 'Search Charts', 'prc-chart-builder' ),
+			'not_found'             => __( 'Not found', 'prc-chart-builder' ),
+			'not_found_in_trash'    => __( 'Not found in Trash', 'prc-chart-builder' ),
+			'featured_image'        => __( 'Chart Thumbnail', 'prc-chart-builder' ),
+			'set_featured_image'    => __( 'Set chart thumbnail', 'prc-chart-builder' ),
+			'remove_featured_image' => __( 'Remove chart thumbnail', 'prc-chart-builder' ),
+			'use_featured_image'    => __( 'Use as chart thumbnail', 'prc-chart-builder' ),
+			'insert_into_item'      => __( 'Insert into Chart', 'prc-chart-builder' ),
+			'uploaded_to_this_item' => __( 'Uploaded to this Chart', 'prc-chart-builder' ),
+			'items_list'            => __( 'Charts list', 'prc-chart-builder' ),
+			'items_list_navigation' => __( 'Charts list navigation', 'prc-chart-builder' ),
+			'filter_items_list'     => __( 'Filter Chart list', 'prc-chart-builder' ),
+		);
 	}
 
+	/**
+	 * Register the post type.
+	 */
 	public function register_types() {
-		$rewrite = [
+		$rewrite = array(
 			'slug'  => 'chart',
 			'pages' => false,
-		];
-		$args    = [
-			'label'               => __( 'Chart', PRC_CHART_BUILDER_NAMESPACE ),
-			'description'         => __( 'A store for chart blocks. This post type allows you to save a chart once and update everywhere it is used.', PRC_CHART_BUILDER_NAMESPACE ),
+		);
+		$args    = array(
+			'label'               => __( 'Chart', 'prc-chart-builder' ),
+			'description'         => __( 'A store for chart blocks. This post type allows you to save a chart once and update everywhere it is used.', 'prc-chart-builder' ),
 			'labels'              => self::get_labels(),
-			'supports'            => ['title', 'editor', 'excerpt', 'author', 'thumbnail', 'revisions', 'custom-fields' ],
-			'taxonomies'          => ['category'],
+			'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'revisions', 'custom-fields' ),
+			'taxonomies'          => array( 'category' ),
 			'hierarchical'        => false,
 			'public'              => true,
 			'show_ui'             => true,
 			'show_in_menu'        => true,
-			'menu_icon'		      => self::$menu_icon,
-			'menu_position'       => 5,
+			'menu_icon'           => self::$menu_icon,
+			'menu_position'       => 10,
 			'show_in_admin_bar'   => true,
 			'show_in_nav_menus'   => true,
 			'show_in_rest'        => true,
@@ -67,19 +89,22 @@ class Content_Type {
 			'publicly_queryable'  => true,
 			'rewrite'             => $rewrite,
 			'capability_type'     => 'post',
-			'template'			  => [['prc-block/chart-builder-controller']],
-		];
+			'template'            => array( array( 'prc-block/chart-builder-controller' ) ),
+		);
 
 		register_post_type( self::$post_type, $args );
 	}
 
-	public function enable_gutenberg_ramp($post_types) {
-		array_push($post_types, self::$post_type);
-		return $post_types;
-	}
-
 	/**
+	 * Modify the oEmbed response.
+	 *
 	 * @hook oembed_response_data
+	 *
+	 * @param mixed $data
+	 * @param mixed $post
+	 * @param mixed $width
+	 * @param mixed $height
+	 * @return mixed
 	 */
 	public function modify_oembed_response( $data, $post, $width, $height ) {
 		// Doing a little bit of checking here to make sure we're only modifying the oEmbed response for the chart post type.
