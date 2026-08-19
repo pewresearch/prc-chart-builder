d9f565625944affd505a8d0c9b7d76c4ace96980
prcdevgitbot <webdev@pewresearch.org>
2026-08-19 22:51:16 +0000

    See https://github.com/pewresearch/prc-platform/commit/c9f968b6049745013a112db9775484f9b775ad64 from refs/tags/v1.13.6

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'prc/chart-builder',
         'pretty_version' => 'dev-trunk',
         'version' => 'dev-trunk',
-        'reference' => 'e88178a34cba82d008b11862a38d008b759664cf',
+        'reference' => 'bfc7e9bcaca3ed6ea1eb4bb36331395beb9c1ad9',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -148,7 +148,7 @@
         'prc/chart-builder' => array(
             'pretty_version' => 'dev-trunk',
             'version' => 'dev-trunk',
-            'reference' => 'e88178a34cba82d008b11862a38d008b759664cf',
+            'reference' => 'bfc7e9bcaca3ed6ea1eb4bb36331395beb9c1ad9',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
